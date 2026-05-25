599957696e726a3de6a9999a2dce49e424d9f7b7
bhavith07 <Bhavithb64@gmail.com>
2026-05-25 21:00:07 +0530

    added api integration and

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
+import "../css/Navbar.css"
 
-function NavBar(){
+function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
             <Link to="/">Movie App</Link>
